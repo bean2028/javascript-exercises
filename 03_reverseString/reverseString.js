b55e220a1d659fad5be0ba7985d681e2a33995c5
Eric Helstad <erichelstad@gmail.com>
2022-08-22 20:50:54 -0500

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = Array.from(string);
+    let reverseArray = stringArray.reverse();
+    let newString = reverseArray.join("");
+    return newString;
 };
 
 // Do not edit below this line
