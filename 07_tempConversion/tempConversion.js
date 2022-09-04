196a87a150e2df7253b98893c2101077adb9f260
Eric Helstad <erichelstad@gmail.com>
2022-09-04 17:40:13 -0500

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  newTemp = ((fTemp - 32) * (5 / 9));
+  return Math.round(newTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  newTemp = (cTemp * (9 / 5)) + 32;
+  return Math.round(newTemp * 10) / 10;
 };
 
 // Do not edit below this line
