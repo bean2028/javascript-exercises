d08abd5f3fb6078c956b40c88550f304e376ab6d
Eric Helstad <erichelstad@gmail.com>
2022-09-04 12:14:14 -0500

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return 'ERROR';
+    }
+    else if (x < 0 ) {
+        return 'ERROR';
+    }
+    else if (x < y) {
+        for (let i = x; i <= y; i++) {
+            sum += i;
+        }
+    } 
+    else {
+        for (let i = x; i >= y; i--) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
