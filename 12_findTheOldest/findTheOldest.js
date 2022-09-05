a617f6d6989bb4a020ed9f4ffd2ab2b2d9e0a161
Eric Helstad <erichelstad@gmail.com>
2022-09-05 12:42:55 -0500

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(input) {
+    const myDate = new Date();
+    let currentYear = myDate.getFullYear();
+    for (let i = 0; i < input.length; i++) {
+        if (!input[i].yearOfDeath) {
+            input[i].yearOfDeath = currentYear;
+        }
+        input[i].age = input[i].yearOfDeath - input[i].yearOfBirth;
+    }
+    answer = input.sort(function(a, b) {return b.age - a.age});
+    return answer[0];
 };
 
 // Do not edit below this line
