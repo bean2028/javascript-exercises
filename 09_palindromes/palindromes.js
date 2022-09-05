22f1292029cf4ce2c616eb1c3082e383eeb81132
Eric Helstad <erichelstad@gmail.com>
2022-09-05 09:48:35 -0500

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.toLowerCase();
+    input = input.replace(/[^a-z0-9]/gi, "");
+    let reverse = input.split("").reverse().join("");
+    if (input === reverse) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
