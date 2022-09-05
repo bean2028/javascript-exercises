ecf73d3f597e058e725229ea8cc127a536057d4d
Eric Helstad <erichelstad@gmail.com>
2022-09-05 10:41:08 -0500

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibNum) {
+    let fib1 = 0;
+    let fib2 = 1;
+    let next = null;
+    let fibSeq = [];
+    for (let i = 0; i <= fibNum; i++) {
+        fibSeq.push(fib1);
+        next = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = next;
+    }
+    let answer = fibSeq[fibNum];
+    if (Number.isInteger(answer) === true) {
+        return answer;
+    }
+    else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
